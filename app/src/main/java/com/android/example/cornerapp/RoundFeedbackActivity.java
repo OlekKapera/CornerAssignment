74505d98624901c3c0c9c218e85d6b4140072d56
Aleksander Kapera <kapera@localhost.company>
2018-04-17 20:29:50 +0200

    Init android commit

---
+++ app/src/main/java/com/android/example/cornerapp/RoundFeedbackActivity.java
@@ -0,0 +1,13 @@
+package com.android.example.cornerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoundFeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round_feedback);
+    }
+}
